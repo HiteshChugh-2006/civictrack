8858c918ae434299479534297ca6109e2ab2a323
HiteshChugh-2006 <hiteshchugh101@gmail.com>
2026-04-24 09:32:37 +0530

    Update MapView.js

--- client/src/pages/MapView.js
+++ client/src/pages/MapView.js
@@ -1,11 +1,20 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
+import L from "leaflet";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
 import "leaflet/dist/leaflet.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-// 🔥 FIX ICON ISSUE
+// Fix default icon issue
 delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 const createIcon = (color) =>
   new L.Icon({
