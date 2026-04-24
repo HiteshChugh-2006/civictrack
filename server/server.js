084e2ecdb033d326c4c54a16b8eb13a5bb828809
HiteshChugh-2006 <hiteshchugh101@gmail.com>
2026-04-24 09:17:31 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -32,6 +32,6 @@ const path = require("path");
 // Serve frontend
 app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
