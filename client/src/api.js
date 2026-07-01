bfdf79fb7f8b3369b0d79baac838f8e9d6cd0347
HiteshChugh-2006 <hiteshchugh2006@gmail.com>
2026-07-01 14:46:41 +0530

    fix: dynamically target backend server port 5000 in local development

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api"
+  baseURL: window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
+    ? "http://localhost:5000/api"
+    : "/api"
 });
 
 api.interceptors.request.use((config) => {
